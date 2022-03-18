832bc08c329ea423daf6ede8aac62ec7c51c36d4
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2022-03-18 14:35:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React from "react";
 
 const App = () => {
-  
   return (
-
-  )
-}
-
+    <div className="container">
+      <form>
+        <label>Name</label>
+        <input type="text" id="name" />
+        <br />
+        <label>Email</label>
+        <input type="text" id="email" />
+        <br />
+        <label>Password</label>
+        <input type="password" id="password" />
+        <br />
+        <label>Consent</label>
+        <input type="checkbox" />
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
